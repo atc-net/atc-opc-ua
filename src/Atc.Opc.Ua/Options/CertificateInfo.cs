5f063ae796727ff54fe54388d2d6222e9b3a4bd7
Per Kops <pko@delegate.dk>
2023-10-10 14:45:36 +0200

    feat: Introducing OpcUaSecurityOptions

---
+++ src/Atc.Opc.Ua/Options/CertificateInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Atc.Opc.Ua.Options;
+
+/// <summary>
+/// Certificate information
+/// </summary>
+public class CertificateInfo : CertificateStore
+{
+    /// <summary>
+    /// Subject name
+    /// </summary>
+    public string? SubjectName { get; set; }
+}
---
+++ src/Atc.Opc.Ua/Options/OpcUaSecurityOptions.cs
@@ -0,0 +1,71 @@
+﻿namespace Atc.Opc.Ua.Options;
+
+public sealed class OpcUaSecurityOptions
+{
+    /// <summary>
+    /// PkiRootPath.
+    /// </summary>
+    public string PkiRootPath { get; set; } = "opc/pki";
+
+    /// <summary>
+    /// Application Certificate.
+    /// </summary>
+    public CertificateInfo ApplicationCertificate { get; set; } = new()
+    {
+        StoreType = CertificateStoreType.Directory,
+        StorePath = "own",
+    };
+
+    /// <summary>
+    /// Rejected certificates.
+    /// </summary>
+    public CertificateStore RejectedCertificates { get; set; } = new()
+    {
+        StoreType = CertificateStoreType.Directory,
+        StorePath = "rejected",
+    };
+
+    /// <summary>
+    /// Trusted issuer certificates.
+    /// </summary>
+    public CertificateStore TrustedIssuerCertificates { get; set; } = new()
+    {
+        StoreType = CertificateStoreType.Directory,
+        StorePath = "issuers",
+    };
+
+    /// <summary>
+    /// Trusted peer certificates.
+    /// </summary>
+    public CertificateStore TrustedPeerCertificates { get; set; } = new()
+    {
+        StoreType = CertificateStoreType.Directory,
+        StorePath = "trusted",
+    };
+
+    /// <summary>
+    /// Automatically add application certificate to the trusted store.
+    /// </summary>
+    public bool AddAppCertToTrustedStore { get; set; } = true;
+
+    /// <summary>
+    /// Whether to auto accept untrusted certificates.
+    /// </summary>
+    public bool AutoAcceptUntrustedCertificates { get; set; }
+
+    /// <summary>
+    /// Minimum key size.
+    /// </summary>
+    public ushort MinimumCertificateKeySize { get; set; } = 1024;
+
+    /// <summary>
+    /// Whether to reject unsecure signatures.
+    /// </summary>
+    public bool RejectSha1SignedCertificates { get; set; } = true;
+
+    /// <summary>
+    /// Reject chain validation with CA certs with unknown revocation status,
+    /// e.g.when the CRL is not available or the OCSP provider is offline.
+    /// </summary>
+    public bool RejectUnknownRevocationStatus { get; set; } = true;
+}
