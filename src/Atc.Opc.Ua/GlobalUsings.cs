f7460aa0f31e6418ca569fa7006e4ced871a43e9
Per Kops <pko@delegate.dk>
2023-10-10 14:52:13 +0200

    feat: Adding GlobalUsings

--- src/Atc.Opc.Ua/GlobalUsings.cs
+++ src/Atc.Opc.Ua/GlobalUsings.cs
@@ -6,12 +6,13 @@ global using System.Runtime.Serialization;
 global using Atc.Opc.Ua.Contracts;
 global using Atc.Opc.Ua.Extensions;
 global using Atc.Opc.Ua.Factories;
+global using Atc.Opc.Ua.Options;
 global using Atc.Opc.Ua.Protocol.Mappers;
 global using Atc.Opc.Ua.Protocol.Runtime;
 global using Atc.Opc.Ua.Protocol.Runtime.Extensions;
 
 global using Microsoft.Extensions.Logging;
-
+global using Microsoft.Extensions.Options;
 global using Opc.Ua;
 global using Opc.Ua.Client;
 global using Opc.Ua.Client.ComplexTypes;
