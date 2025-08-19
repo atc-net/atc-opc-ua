c579f8a3d91425cff5ae08b381bda96339284955
Per Kops <pko@delegate.dk>
2025-08-19 08:58:17 +0200

    fix: ensure proper traversal for child NodeVariables in NodeTreeDiffer

--- test/Atc.Opc.Ua.Tests/Differs/NodeTreeDifferTests.cs
+++ test/Atc.Opc.Ua.Tests/Differs/NodeTreeDifferTests.cs
@@ -2,6 +2,19 @@ namespace Atc.Opc.Ua.Tests.Differs;
 
 public sealed class NodeTreeDifferTests
 {
+    private readonly JsonSerializerOptions fileSerializerOptions;
+
+    public NodeTreeDifferTests()
+    {
+        fileSerializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+
+        fileSerializerOptions.Converters.Add(new NodeJsonConverter());
+    }
+
     [Fact]
     public void Diff_Should_Handle_Nulls()
     {
@@ -49,4 +62,33 @@ public sealed class NodeTreeDifferTests
         // added should include 30
         result.AddedNodes.Select(n => n.NodeId).Should().BeEquivalentTo("ns=2;i=30");
     }
+
+    [Fact]
+    public void Diff_Should_Find_Nested_Added_And_Deleted_And_Unchanged()
+    {
+        // Arrange
+        var previousScanResultJson = TestResourceProvider.GetTestData("001_scan_previous_result");
+        var currentScanResultJson = TestResourceProvider.GetTestData("001_scan_current_result");
+
+        var previous = JsonSerializer.Deserialize<OpcUaScanResult>(previousScanResultJson, fileSerializerOptions);
+        var current = JsonSerializer.Deserialize<OpcUaScanResult>(currentScanResultJson, fileSerializerOptions);
+
+        // Act
+        var result = NodeTreeDiffer.Diff(current.Result, previous.Result);
+
+        // Assert
+        Assert.NotNull(result);
+
+        Assert.Single(result.AddedNodes);
+        Assert.Equal(20, result.DeletedNodes.Count);
+    }
 }
+
+#pragma warning disable MA0048
+internal sealed record OpcUaScanResult( // TODO: 
+#pragma warning restore MA0048
+    string EndpointUrl, // TODO: 
+    string StartingNodeId,
+    NodeBase Result,
+    DateTimeOffset Start,
+    DateTimeOffset End);
